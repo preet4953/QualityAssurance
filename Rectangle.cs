a79a698428c328459fd43ccb42a7808951fcab13
preet4953 <preet4953@gmail.com>
2021-10-05 19:55:50 -0400

    Containing the completed Rectangle class

---
+++ Rectangle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace As2ConsoleApp
+{
+    class Rectangle
+    {
+            private int length,width,height;
+
+            public Rectangle()
+            {
+                this.length = 1;
+                this.width = 1;
+                this.height = 1;
+            }
+
+            public Rectangle(int length, int width, int height)
+            {
+                this.length = length;
+                this.width = width;
+                this.height = height;
+            }
+
+            public int GetLength()
+            {
+                return this.length;
+            }
+
+            public void SetLength(int length)
+            {
+                this.length =length;
+            }
+
+            public int GetWidth()
+            {
+                return this.width;
+            }
+
+            public void SetWidth(int width)
+            {
+                this.width =width;
+            }
+
+            public int GetHeight()
+            {
+                return this.height;
+            }
+
+            public void SetHeight(int height)
+            {
+                this.height =height;
+            }
+
+            public int GetVolume()
+            {
+            return this.length * this.width * this.height; 
+            }
+    }
+}
+
